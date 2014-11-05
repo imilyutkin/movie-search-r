efec0317ebe1fe90dd6d0f34c11fbc3a68bf617d
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2014-11-05 22:38:50 +0300

    added empty start page and updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/app/src'))
+app.use(express.static(__dirname + '/src'))
 app.set('port', (process.env.PORT || 5000))
 
 app.get('/', function (req, res) {
