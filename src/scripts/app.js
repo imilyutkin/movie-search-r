e6a1722eda4777037324cf41fdaaa650bf396db7
imilyutkin <ivan.milyutkin@gmail.com>
2014-11-12 16:28:12 +0300

    added list for movies

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,18 @@
 var data = [
-
+  {name: "Ivan Milyutkin", subject: "Bla bla bla", avatar:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name: "Ivan Milyutkin", subject: "Bla bla bla", avatar:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name: "Ivan Milyutkin", subject: "Bla bla bla", avatar:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name: "Ivan Milyutkin", subject: "Bla bla bla", avatar:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name: "Ivan Milyutkin", subject: "Bla bla bla", avatar:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name: "Ivan Milyutkin", subject: "Bla bla bla", avatar:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  {name: "Ivan Milyutkin", subject: "Bla bla bla", avatar:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }
 ];
 
 var MovieItem = React.createClass({
   render: function() {
     return (
          <div className="movie-item movie-item-selected pure-g">
-                <div class="pure-u">
+                <div className="pure-u">
                     <img className="movie-avatar" height="64" width="64" src={this.props.avatar}/>
                 </div>
 
@@ -14,7 +20,7 @@ var MovieItem = React.createClass({
                     <h5 className="movie-name">{this.props.name}</h5>
                     <h4 className="movie-subject">{this.props.subject}</h4>
                     <p className="movie-desc">
-                    {this.props.children}
+                    {this.props.text}
                     </p>
                 </div>
             </div>
@@ -23,14 +29,15 @@ var MovieItem = React.createClass({
 });
 
 var MovieList = React.createClass({
-    render: function(){
-
-    }
+  render: function() {
+    var renderNodes = this.props.data.map(function(movie) {
+      return ( <MovieItem name={movie.name} subject={movie.subject} avatar={movie.avatar} text={movie.text} /> );
+    });
+    return  (<div id="res">{renderNodes}</div>);
+  }
 });
 
 React.render(
-  <MovieItem name="Ivan Milyutkin" subject="Bla bla bla" avatar="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrmD5lfBINhjJJw_iPJGm5JCQc9EWAZGLt96GnpsGWxqdqbXOjXe_1f64">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-  </MovieItem>,
+  <MovieList data={data} />,
   document.getElementById('list')
 );
