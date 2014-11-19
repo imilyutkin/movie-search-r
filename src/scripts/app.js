e50fbe01f931b69a0184a42781504fb922093685
imilyutkin <ivan.milyutkin@gmail.com>
2014-11-19 20:28:19 +0300

    added based view

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -12,11 +12,11 @@ var MovieItem = React.createClass({
   render: function() {
     return (
          <div className="movie-item movie-item-selected pure-g">
-                <div className="pure-u">
-                    <img className="movie-avatar" height="64" width="64" src={this.props.avatar}/>
+                <div className="pure-u-2-5 center">
+                    <img className="movie-avatar" height="138" width="92" src={"http://image.tmdb.org/t/p/w92/" + this.props.avatar}/>
                 </div>
 
-                <div className="pure-u-3-4">
+                <div className="pure-u-3-5">
                     <h5 className="movie-name">{this.props.name}</h5>
                     <h4 className="movie-subject">{this.props.subject}</h4>
                     <p className="movie-desc">
@@ -29,15 +29,30 @@ var MovieItem = React.createClass({
 });
 
 var MovieList = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function(){
+    this.loadMoviesFromServer();
+    // setInterval(this.loadMoviesFromServer, this.props.pollInterval);
+  },
+  loadMoviesFromServer: function() {
+    var self = this;
+    $.getJSON("https://api.themoviedb.org/3/movie/upcoming?api_key=c2c73ebd1e25cbc29cf61158c04ad78a",
+        function(data){
+          console.log(data.results)
+          self.setState({data: data.results});
+        });
+  },
   render: function() {
-    var renderNodes = this.props.data.map(function(movie) {
-      return ( <MovieItem name={movie.name} subject={movie.subject} avatar={movie.avatar} text={movie.text} /> );
+    var renderNodes = this.state.data.map(function(movie) {
+      return ( <MovieItem name={movie.title} subject={movie.subject} avatar={movie.poster_path} text={movie.text} /> );
     });
     return  (<div id="res">{renderNodes}</div>);
   }
 });
 
 React.render(
-  <MovieList data={data} />,
+  <MovieList data={data} pollInterval={2000}/>,
   document.getElementById('list')
 );
